ad7ab19a13bddfd4eaef9083e44ab3fb099568ec
aravi123 <aravindcranjan54@gmail.com>
2016-11-04 15:54:12 +0530

    Changed Structure of the page ...added Username and current connected users

--- app.js
+++ app.js
@@ -4,83 +4,113 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongoose=require('mongoose');
 
-var from = "",to = "",username="";
-
+var from = "",to = "",usernames=[];
+var z= 0;
 var date = new Date();
 
 app.set('view engine','ejs');
 
-mongoose.connect('mongodb://localhost/cht');
-var db= mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("Database connected");
-});
+// mongoose.connect('mongodb://localhost/cht');
+// var db= mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log("Database connected");
+// });
 
-var chatschema = mongoose.Schema({
-  time:String,
-  from:String,
-  to:String,
-  message:String
-});
+// var chatschema = mongoose.Schema({
+//   time:String,
+//   from:String,
+//   to:String,
+//   message:String
+// });
 
-var connectedusers = mongoose.Schema({
-  name:String
-});
+// var connectedusers = mongoose.Schema({
+//   name:String,
+//   ids:String
+// });
 
-var chat = mongoose.model('chatdata',chatschema);
-var users = mongoose.model('ConnectedUsers',connectedusers);
+// var chat = mongoose.model('chatdata',chatschema);
+// var users = mongoose.model('ConnectedUsers',connectedusers);
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function(req, res){
-  res.render('../index.ejs',{userdetails:""});
+  res.render('../index.ejs');
 });
- app.post('/username',function(req,res){
-  console.log(req.body.name);
-  username = req.body.name;
-  var user = new users({name:username});
-  user.save(function(error,sucess){
-    if(error){
-      console.log(error);
-    }
-    else
-    {
-      console.log("sucess");
-    }
-  });
-});
+ 
+// app.post('/socketid',function(req,res){
+//   console.log(req.body.id);
+//   console.log(req.body.name);
+//   var user = new users({name:req.body.name,ids:req.body.id});
+//   user.save(function(err,sucess){
+//     if (err) {
+//       console.log("err");
+//     }
+//     else{
+//       console.log("sucess");
+//       z=z+1;
+//     }
+//   });
+// });
 
-app.get('/admin/:username',function(req,res){
-  var userName=req.params.username;
-  console.log(userName);
-});
+// app.get('/admin/:username',function(req,res){
+//   to=req.params.username;
+//   res.render('../index.ejs');
+// });
 
-app.get('/connectedusers',function(req,res){
-  console.log("called");
-  users.find(function(err,usersdetails){
-    if (err) {
-      console.log("error");
-    }
-    else{
-      console.log(usersdetails);
-      res.render('../index.ejs',{userdetails:usersdetails});
-    }
-  });
-});
+// app.get('/connectedusers',function(req,res){
+//   console.log("called");
+//   users.find(function(err,usersdetails){
+//     if (err) {
+//       console.log("error");
+//     }
+//     else{
+//       console.log(usersdetails);
+//       res.render('../index.ejs',{userdetails:usersdetails});
+//     }
+//   });
+// });
 
-io.on('connection', function(socket){
+io.sockets.on('connection', function(socket){
+   // console.log(to);
+   //  users.find({name:to},function(err,user){
+   //    if (err) {
+   //      console.log("error");
+   //    }
+   //    else
+   //    {
+   //      //console.log(user[0].ids);
+   //      //socket.broadcast.to(user[0].ids).emit('chat message',msg);
+   //    }
+   //  });
   socket.on('disconnect',function(){
+    if (!socket.usernames)return;
+    usernames.splice(usernames.indexOf(socket.usernames),1);
+    io.sockets.emit('usernames',usernames);
+  });
+  socket.on('id',function(msg){
+    console.log(msg);
   });
   socket.on('chat message', function(msg){
-    io.emit('chat message',msg);
+    io.sockets.emit('chat message',{msg:msg,username:socket.usernames});
   });
   socket.on('typing',function(msg){
     socket.broadcast.emit('typing',"user is typing");
   });
+  socket.on('username',function(data,callback){
+    if (usernames.indexOf(data)!=-1) {
+      callback(false);
+    }
+    else{
+      callback(true);
+      socket.usernames=data;
+      usernames.push(socket.usernames);
+      io.sockets.emit('usernames',usernames);
+    }
+  });
 
 });
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+  
+http.listen(3000,function(){
+ console.log('listening on *:3000');
 });
